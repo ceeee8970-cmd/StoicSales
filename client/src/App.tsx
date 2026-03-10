7ccef73f5028446c653329197f788eb3bab381c8
ceeee8970-cmd <ceeee8970@gmail.com>
2026-03-10 21:10:06 +0100

    Import Router from wouter in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
